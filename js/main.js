959ec563084c1a562329b78385691c36cf1165be
RomanMalinovskiy <rrcolab@yandex.ru>
2022-02-22 10:35:50 +0300

    Revert "Выполняю задание №2 (Функции добавляю) в файл main.js"
    This reverts commit a924a380dd6d8c7e6935aa3cef6b74c81cf37c1c.
    
    # Conflicts:
    #	js/main.js

--- js/main.js
+++ js/main.js
@@ -1,30 +1,31 @@
-/*Функция, возвращающая случайное целое число из переданного диапазона включительно.
-Пример использования функции:
+function getRandomInRange(minNumber, maxNumber) {
+  if (minNumber >= 0 && maxNumber >= minNumber) {
+    return Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
+  } else {
+    return 'Invalid Input Parameters';
+  }
+}
+getRandomInRange(0, 3);
 
-имя_функции(от, до); // Результат: целое число из диапазона "от...до"
-
-Учтите, что диапазон может быть только положительный, включая ноль.
-
-А также придумайте, как функция должна вести себя,
-если передать значение «до» меньшее, чем значение «от», или равное ему.
-.*/
-
-function randomInteger(mIn, mAx, numOfDecimalPlaces) {
-  if (mIn >= 0 && mAx >= mIn) {
-    const diapason = (mAx - mIn);
-    return Number(diapason + 1 / Math.pow(10, numOfDecimalPlaces + 1)).toFixed(numOfDecimalPlaces);
+function randomInteger(minNumber, maxNumber, numOfDecimalPlaces) {
+  if (minNumber >= 0 && maxNumber >= minNumber) {
+    const range = (maxNumber - minNumber);
+    return Number(range + 1 / Math.pow(10, numOfDecimalPlaces + 1)).toFixed(numOfDecimalPlaces);
   }
   return 'Invalid Input Parameters';
 }
-randomInteger(3, 4,0);
+/*
+Функция, возвращающая случайное число с плавающей точкой из переданного диапазона включительно. Будет использоваться для генерации временных географических координат в следующем задании. Пример использования функции:
 
-/*Д15. Если при использовании условного оператора в любом случае возвращается значение,
-альтернативная ветка опускается.
-Д17. Там, где возможно, в присвоении значения вместо if используется тернарный оператор.*/
+  имя_функции(от, до, количество_знаков_после_запятой); // Результат: число с плавающей точкой из диапазона "от...до" с указанным "количеством знаков после запятой"
+Учтите, что диапазон может быть только положительный, включая ноль. А также придумайте, как функция должна вести себя, если передать значение «до» меньшее, чем значение «от», или равное ему. Не забудьте, что в случае с дробными числами диапазон может быть в десятых, сотых, тысячных и т. д. долях. Например, 1.1, 1.2 — корректный диапазон.
+*/
+randomInteger(11, 15.6, 1);
 
-/*function randomInteger(mIn, mAx, numOfDecimalPlaces) {
-  (mIn >= 0 && mAx >= mIn) ?
-  const diapason = (mAx - mIn) : Number(diapason + 1 / Math.pow(10, numOfDecimalPlaces + 1)).toFixed(numOfDecimalPlaces)
-  return 'Invalid Input Parameters';
+
+function returnRandomNum(minNumber, maxNumber, numOfDecimalPlaces) {
+  const range = (minNumber - maxNumber);
+  return (minNumber >= 0 && maxNumber >= minNumber) ?
+    range + 1 / Math.pow(10, numOfDecimalPlaces + 1).toFixed(numOfDecimalPlaces) : 'Invalid Input Parameters';
 }
-console.log(randomInteger(1, 6,0));*/
+returnRandomNum(10.5, 14.6, 1);
