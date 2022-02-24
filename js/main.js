d35e7c87cab8263b1525951b880484ce5644bbcc
RomanMalinovskiy <rrcolab@yandex.ru>
2022-02-24 16:44:12 +0300

    Внес исправления и замечания gis status

--- js/main.js
+++ js/main.js
@@ -1,25 +1,16 @@
 function getRandomInRange(minNumber, maxNumber) {
   if (minNumber >= 0 && maxNumber >= minNumber) {
-    return Math.floor(Math.random() * (maxNumber - minNumber)) + minNumber;
+    return Math.round(Math.random() * (maxNumber - minNumber)) + minNumber;
   } else {
-    return 'Invalid Input Parameters';
+    throw new Error('Invalid Input Parameters');
   }
 }
-getRandomInRange(0, 3);
+getRandomInRange(0, 6);
 
-function randomInteger(minNumber, maxNumber, numOfDecimalPlaces) {
-  if (minNumber >= 0 && maxNumber >= minNumber) {
-    const range = (maxNumber - minNumber);
-    return Number(range + 1 / Math.pow(10, numOfDecimalPlaces + 1)).toFixed(numOfDecimalPlaces);
-  }
-  return 'Invalid Input Parameters';
-}
-randomInteger(11, 15.6, 1);
 
-/*Переделал вторую функцию под тернарный оператор, но не могу понять в чем ошибка отрицательное число выводит функция */
 function returnRandomNum(minNumber, maxNumber, numOfDecimalPlaces) {
-  const range = (minNumber - maxNumber);
+  const range = (maxNumber - minNumber);
   return (minNumber >= 0 && maxNumber >= minNumber) ?
-    Number (range + 1 / Math.pow(10, numOfDecimalPlaces + 1)).toFixed(numOfDecimalPlaces) : 'Invalid Input Parameters';
+    Number ( Math.random(range) + 1 / Math.pow(10, numOfDecimalPlaces + 1)).toFixed(numOfDecimalPlaces) : 'Invalid Input Parameters';
 }
-returnRandomNum(10.5, 14.6, 1);
+returnRandomNum(1.1, 1.2, 3);
